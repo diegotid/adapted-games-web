db6d0eff5426db63739b5813b47aa5a6f345cfa1
Diego <Diego@MacBook-Pro.local>
2016-08-14 00:46:01 +0200

    Puzzle - Physics improvements

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -2,34 +2,171 @@
 var activeTile;
 var gameFinished;
 var selectedImage;
+
+var gameLevel = 1;
 var imageScale = 1;
 var imageTiles = [];
+var finishedTiles = [];
 
 var PAGE_MARGIN = 25;
-var MAGNET_AREA = 25;
+var MAGNET_AREA = 50;
 
-function startGame(imageFile) {
-  imageScale = 0.75;
+function setup() {
+  document.getElementById("level").style.display = 'none';
+  var dropZone = document.getElementsByClassName("gamesetup")[0];
+  selectedImage = new Image();
+  if (window.FileReader) {
+    document.body.addEventListener('dragover', function(e) {
+      e.preventDefault();
+    }, false);
+    document.body.addEventListener('dragenter', function(e) {
+      e.preventDefault();
+      dropZone.className = "gamesetup";
+    }, false);
+    document.body.addEventListener('drop', function(e) {
+      e.preventDefault();
+    }, false);
+    dropZone.addEventListener('dragover', function(e) {
+      e.preventDefault();
+    }, false);
+    dropZone.addEventListener('dragenter', function(e) {
+      dropZone.className += " dragovercolor";
+      e.stopPropagation();
+    }, false);
+    dropZone.addEventListener('drop', function(e) {
+      e.preventDefault();
+      dropZone.className = "gamesetup";
+      var file = e.dataTransfer.files[0];
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.addEventListener('loadend', function(e, file) {
+        selectImage(this.result);
+        showGalery();
+      });
+    }, false);
+  } else {
+    dropZone.innerHTML = 'Navegador no soportado';
+  }
+}
+
+function selectImage(source) {
+  selectedImage.src = source;
+  document.getElementById("imagepreview").src = selectedImage.src;
+  imageScale = (window.innerWidth / selectedImage.width) / 2;
+  if (window.innerHeight / selectedImage.height < imageScale * 2) {
+    imageScale = (window.innerHeight / selectedImage.height) / 2;
+  }
+  changeLevel(document.getElementById("levelselector").value);
+  document.getElementById("altoptions").className = 'crowded';
+  document.getElementById("level").style.display = 'inline';
+  showGalery();
+}
+
+function showGalery() {
+  var galery = document.getElementById('galery');
+  if (galery.style.display == 'inline') {
+    galery.style.display = 'none';
+    document.getElementById("imagepreview").style.display = 'block';
+  } else {
+    galery.style.display = 'inline';
+    document.getElementById("imagepreview").style.display = 'none';
+  }
+}
+
+function changeLevel(level) {
+  var level = parseInt(level);
+  var verbo = document.getElementById("selectlevel");
+  switch (level) {
+    case 0:
+      verbo.innerHTML = "Nivel: F&aacute;cil";
+      break;
+    case 1:
+      verbo.innerHTML = "Nivel: Moderado";
+      break;
+    case 2:
+      verbo.innerHTML = "Nivel: Dif&iacute;cil";
+      break;
+    case 3:
+      verbo.innerHTML = "Nivel: Muy dif&iacute;cil";
+      break;
+    default:
+      verbo.innerHTML = "Selecciona nivel de dificultad";
+  }
+  gameLevel = 4 + 2 * level;
+  imageTiles.length = 0;
+  var cols = Math.round(selectedImage.width * imageScale / (window.innerWidth / gameLevel));
+  var rows = Math.round(selectedImage.height * imageScale / (window.innerHeight / gameLevel));
+  var celw = selectedImage.width / cols;
+  var celh = selectedImage.height / rows;
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      imageTiles[i * rows + j] = new component(selectedImage.src, i * celw, j * celh, celw, celh);
+    }
+  }
+}
+
+function startGame() {
   document.ontouchmove = function(event) {
       event.preventDefault();
   }
-  selectedImage = new Image();
-  selectedImage.src = imageFile;
-  imageTiles[0] = new component(imageFile, 0, 0, selectedImage.width, selectedImage.height / 2);
-  imageTiles[1] = new component(imageFile, 0, selectedImage.height / 2, selectedImage.width, selectedImage.height / 2);
   gameFinished = false;
   gameArea.start();
+  document.getElementsByClassName("back")[0].style.display = 'inline';
+  document.getElementsByClassName("reload")[0].style.display = 'inline';
+  document.getElementsByClassName("gamesetup")[0].style.display = 'none';
+  document.getElementById("gamename").className = 'crowded';
 }
 
 function restartGame() {
   for (var i = 0; i < imageTiles.length; i++) {
     imageTiles[i].restart();
   }
+  finishedTiles.length = 0;
   gameFinished = false;
 }
 
-function endGame() {
-  gameArea.end();
+function settle(settled, tile) {
+  for (var i = 0; i < finishedTiles.length; i++) {
+    var set = finishedTiles[i];
+    if (set.indexOf(settled) >= 0) {
+      if (set.indexOf(tile) < 0) {
+        set.push(tile);
+      }
+      settleSet(i, tile);
+      return;
+    } else if (set.indexOf(tile) >= 0) {
+      if (set.indexOf(settled) < 0) {
+        set.push(settled);
+      }
+      settleSet(i, settled);
+      return;
+    }
+  }
+  finishedTiles.push(new Array(settled, tile));
+}
+
+function settleSet(setIndex, tile) {
+  for (var i = 0; i < finishedTiles.length; i++) {
+    if (i == setIndex) {
+      continue;
+    }
+    var set = finishedTiles[i];
+    for (var j = 0; j < set.length; j++) {
+      var comp = set[j];
+      if (comp == tile) {
+        continue;
+      }
+      var diff = Math.abs((tile.x - tile.tilex) - (comp.x - comp.tilex)) + Math.abs((tile.y - tile.tiley) - (comp.y - comp.tiley));
+      if (diff < MAGNET_AREA
+      && areBordering(tile, set[j])) {
+        finishedTiles[setIndex] = set.concat(finishedTiles[setIndex]).filter(function(item, pos, self) {
+          return self.indexOf(item) == pos;
+        });
+        finishedTiles.splice(i, 1);
+        break;
+      }
+    }
+  }
 }
 
 var gameArea = {
@@ -45,17 +182,17 @@ var gameArea = {
       window.addEventListener('mousedown', function (e) {
         gameArea.touchEvent(e);
       });
-      window.addEventListener('touchstart', function (e) {
-        gameArea.touchEvent(e);
-      });
-      window.addEventListener('touchend', function (e) {
-        gameArea.touchEvent(e);
-      });
       window.addEventListener('mousemove', function (e) {
         gameArea.moveEvent(e.pageX, e.pageY);
       });
       window.addEventListener('touchmove', function (e) {
         gameArea.moveEvent(e.touches[0].screenX, e.touches[0].screenY);
+      });
+      window.addEventListener('touchstart', function (e) {
+        gameArea.touchEvent(e);
+      });
+      window.addEventListener('touchend', function (e) {
+        gameArea.touchEvent(e);
       });
       gameArea.magnet(imageTiles[0]);
   },
@@ -79,11 +216,14 @@ var gameArea = {
           comp.y = tile.y + (comp.tiley - tile.tiley);
           comp.update();
           diff = Math.abs((tile.x - tile.tilex) - (comp.x - comp.tilex)) + Math.abs((tile.y - tile.tiley) - (comp.y - comp.tiley));
+          if (areBordering(tile, comp)) {
+            settle(tile, comp);
+          }
         }
         total += diff;
       }
     }
-    if (total == 0) {
+    if (total < PAGE_MARGIN) {
       gameFinished = true;
     }
   },
@@ -173,14 +313,43 @@ function component(source, x, y, width, height) {
 
 function updateGameArea() {
   gameArea.clear();
-  var movings = 0;
   for (var i = 0; i < imageTiles.length; i++) {
     var tile = imageTiles[i];
     if (tile.moving && gameArea.x && gameArea.y) {
-      movings++;
+      var x = tile.x - gameArea.x;
+      var y = tile.y - gameArea.y;
       tile.x = gameArea.x;
       tile.y = gameArea.y;
+      dragSettled(tile, x, y);
     }
     tile.update();
   }
+}
+
+function dragSettled(head, x, y) {
+  for (var j = 0; j < finishedTiles.length; j++) {
+    var set = finishedTiles[j];
+    if (set.indexOf(head) >= 0) {
+      for (var i = 0; i < set.length; i++) {
+        var tile = set[i];
+        if (tile != head) {
+          tile.x -= x;
+          tile.y -= y;
+          tile.update();
+        }
+      }
+    }
+  }
+}
+
+function areBordering(a, b) {
+  if (Math.abs(a.tilex - b.tilex) - a.width < 1
+  && Math.abs(a.tiley - b.tiley) < 1) {
+    return true;
+  }
+  if (Math.abs(a.tiley - b.tiley) - a.height < 1
+  && Math.abs(a.tilex - b.tilex) < 1) {
+    return true;
+  }
+  return false;
 }
