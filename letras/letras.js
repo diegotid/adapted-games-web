41191f1537366888c4665f26def98319d65d3548
Diego <Diego@MacBook-Pro.local>
2016-09-01 02:03:08 +0200

    Letras - Introducing spoken word definitions and links to Google Images and YouTube

--- letras/letras.js
+++ letras/letras.js
@@ -2,12 +2,24 @@
 var word;
 var toListen;
 var stack = [];
+var listenTimeout;
+
+var name;
+var allSyllables;
 
 var vowels = [];
 var allVowels = ['a', 'e', 'i', 'o', 'u'];
 var consonants = [];
 
 function setup() {
+  name = getCookie('name');
+  allSyllables = getCookie('syllables');
+  allSyllables = document.getElementById('syllablesCheck').checked = (!allSyllables || allSyllables == 'true');
+  if (name && name.length > 0) {
+    document.getElementById("name").value = name;
+  } else {
+    settings();
+  }
   var alphabet = "abcdefghijklmnñopqrstuvwxyz";
   var alphabetSet = getCookie('alphabet');
   if (!alphabetSet) {
@@ -49,8 +61,13 @@ function setup() {
   for (var i = 0; i < consonants.length; i++) {
     var consonantItem = document.createElement("li");
     consonantItem.innerHTML = consonants[i];
+    consonantItem.setAttribute('class', allSyllables ? '' : 'on');
     consonantItem.addEventListener('click', function(e) {
-      syllable(e.target.innerHTML);
+      if (!allSyllables || e.target.className.length > 0) {
+        addToWord(e.target.innerHTML);
+      } else {
+        syllable(e.target.innerHTML);
+      }
     });
     consonantsList.appendChild(consonantItem);
     var consonantSyllables = document.createElement("ul");
@@ -80,9 +97,22 @@ function setup() {
 }
 
 function addToWord(text) {
+  responsiveVoice.cancel();
+  var substrings = word.innerHTML.split(text);
+  if (substrings.length > 2) {
+    responsiveVoice.speak(name + ", no me tomes el pelo", 'Spanish Female');
+    word.innerHTML = "";
+    stack.length = 0;
+    return;
+  }
   word.innerHTML += text;
   stack.push(text.length);
-  listen(word);
+  if (stack.length > 1) {
+    window.clearTimeout(listenTimeout);
+      listenTimeout = window.setTimeout(function () {
+      listen(word);
+    }, 1500);
+  }
   if (word.clientWidth > 0.75 * word.parentElement.clientWidth) {
     if (word.className) {
       word.className = "small";
@@ -93,7 +123,7 @@ function addToWord(text) {
 }
 
 function setCookie(cname, cvalue) {
-  document.cookie = cname + "=" + cvalue ;
+  document.cookie = cname + "=" + escape(cvalue) + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 }
 
 function getCookie(cname) {
@@ -105,23 +135,33 @@ function getCookie(cname) {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
+      return unescape(c.substring(name.length, c.length));
     }
   }
   return '';
 }
 
-function subset() {
-  var alphabet = document.getElementById("alphabet");
-  alphabet.style.display = (alphabet.style.display != 'block' ? "block" : "none");
-  if (alphabet.style.display == 'none') {
-    location.reload();
+function settings() {
+  var settings = document.getElementById("settings");
+  if (settings.style.display == 'block') {
+    var name = document.getElementById("name").value;
+    var syll = document.getElementById("syllablesCheck");
+    if (name.length > 0) {
+      setCookie('name', name);
+      setCookie('syllables', syll.checked);
+      settings.style.display = 'none';
+      location.reload();
+    } else {
+      responsiveVoice.cancel();
+      responsiveVoice.speak("Por favor, indica el nombre del niño", 'Spanish Female');
+    }
   } else {
-    document.getElementById('rack').style.display = "none";
+    settings.style.display = 'block';
   }
 }
 
 function erase() {
+  responsiveVoice.cancel();
   var lastSyllableLength = stack.pop();
   word.innerHTML = word.innerHTML.substring(0, word.innerHTML.length - lastSyllableLength);
   if (word.clientWidth < 0.5 * word.parentElement.clientWidth) {
@@ -134,12 +174,15 @@ function erase() {
 }
 
 function clean() {
+  responsiveVoice.cancel();
   var sel = window.getSelection();
   sel.removeAllRanges();
   word.innerHTML = "";
+  stack.length = 0;
 }
 
 function listen(what) {
+  window.clearTimeout(listenTimeout);
   responsiveVoice.cancel();
   responsiveVoice.speak(what.innerHTML, 'Spanish Female');
 }
@@ -154,6 +197,47 @@ function syllable(consonant) {
   var consonants = consonantsList.childNodes;
   for (var i = 0; i < consonants.length; i++) {
     var consonantItem = consonants[i];
-    consonantItem.className = (consonantItem.innerHTML == consonant && consonantItem.className.length == 0 ? "on" : "");
+    consonantItem.className = (!allSyllables || (consonantItem.innerHTML == consonant && consonantItem.className.length == 0) ? "on" : "");
   }
+}
+
+function createRequest() {
+  var result = null;
+  if (window.XMLHttpRequest) {
+    result = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    result = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  return result;
+}
+
+function definition(what) {
+  console.log('Definition');
+  var req = createRequest();
+  req.onreadystatechange = function() {
+    if (req.readyState != 4) {
+      return;
+    }
+    if (req.status != 200) {
+      responsiveVoice.cancel();
+      responsiveVoice.speak('Lo siento ' + name + '. Esta palabra creo que no existe', 'Spanish Female');
+      return;
+    }
+    var definicion = JSON.parse(req.responseText)['definicion'];
+    if (definicion) {
+      responsiveVoice.cancel();
+      responsiveVoice.speak('Muy bien ' + name + '. ' + word.innerHTML + ' es: ' + definicion, 'Spanish Female');
+    }
+  };
+  req.open("GET", "http://apicultur.io/api/dicc/1.0.0/definicion/10/" + word.innerHTML, true);
+  req.setRequestHeader('Authorization', 'Bearer St1ir8MlgcjYnEURpORQX1Lohxga');
+  req.send();
+}
+
+function pictures(what) {
+  window.open('https://www.google.es/search?tbm=isch&q=' + what.innerHTML, '_blank');
+}
+
+function videos(what) {
+  window.open('https://www.youtube.com/results?search_query=' + what.innerHTML, '_blank');
 }
